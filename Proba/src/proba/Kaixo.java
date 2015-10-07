c23ec5d848f569e696ee03d67d19365a50fe17aa
isad1516 <isad1516@juanan.juanan.f5.internal.cloudapp.net>
2015-10-07 19:16:31 +0000

    Funtzionalitate berria inplementatzen

--- Proba/src/proba/Kaixo.java
+++ Proba/src/proba/Kaixo.java
@@ -2,6 +2,8 @@ package proba;
 
 public class Kaixo {
 
+	Fitxategi f = new Fitxategi();
+	
 	public static void main(String[] args) {
 		// Komentario bat sartuko dut
 		System.out.println("Kaixo mundua! Hello world");
