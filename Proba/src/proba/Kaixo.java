ef0d319f152e039ae76944026aec32904e463bca
isad1516 <isad1516@juanan.juanan.f5.internal.cloudapp.net>
2015-10-07 17:08:12 +0000

    Beste lerro bat gehitu dut

--- Proba/src/proba/Kaixo.java
+++ Proba/src/proba/Kaixo.java
@@ -5,5 +5,6 @@ public class Kaixo {
 	public static void main(String[] args) {
 		// Komentario bat sartuko dut
 		System.out.println("Kaixo mundua! Hello world");
+		System.out.println("Beste lerro bat");
 	}
 }
